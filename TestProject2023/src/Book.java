86667e8488b8742691881bb985e7df8dcd8a5bc9
E116324 <E116324@localhost>
2023-05-17 13:03:51 +1000

    this first commit

---
+++ TestProject2023/src/Book.java
@@ -0,0 +1,16 @@
+
+public class Book {
+	
+	private int bookID;
+	private String bookName;
+	public String searchBook()
+	{
+		return "";
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
